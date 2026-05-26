f762b2cd9d5d670f8d9c1015d3a3bc72ffd90cc0
epf-21 <efrainperalta.f@gmail.com>
2026-05-26 15:05:18 -0400

    configurar tanstack router

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline" > Home xd</h1>
+    </>
+  );
+}
+
